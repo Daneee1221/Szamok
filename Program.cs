9c28d6a4800aac01272ea3f0e981d10323936509
Ekker Dani <ekkerdani1221@gmail.com>
2019-10-02 21:28:14 +0200

    Feladatok

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Feladat1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>(10);
+            for (int i = 0; i <  10; i++)
+            {
+                Console.WriteLine("Adjon meg egy számot: ");
+                lista.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("A legnagyobb szám: " + lista.Max());
+            Console.ReadKey();
+        }
+    }
+}
